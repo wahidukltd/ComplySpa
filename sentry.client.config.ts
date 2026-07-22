23fbacf170370ba5c9543268155312d1f7cf35ab
wahidukltd <wahidukltd@gmail.com>
2026-07-22 23:41:07 +0500

    chore: remove old sentry.client.config.ts (replaced by instrumentation-client.ts)

--- sentry.client.config.ts
+++
@@ -1,8 +0,0 @@
-import * as Sentry from "@sentry/nextjs";
-
-Sentry.init({
-  dsn: "https://65e972effe4ff5a0b95ab3b8091ecd7e@o4511731049234432.ingest.us.sentry.io/4511779368271872",
-  tracesSampleRate: 0.1,
-  environment: process.env.NODE_ENV,
-  enabled: process.env.NODE_ENV === "production",
-});
